5cf7332d035381472c23d7e423ff90f9b86a2669
Arash Bahramian <bahramian@pa.msu.edu>
2016-12-20 14:44:57 -0500

    Add files via upload

---
+++ gehrels.py
@@ -0,0 +1,57 @@
+#! /astro_sw/anaconda2/bin/python
+# A small code to estimate confidence intervals for low-count events
+# Based on Gehrels+86
+
+import numpy as np
+import sys
+import scipy.stats as sts
+
+
+def gehrels(x,cl=0.8413, step = 0.001):
+    """
+    The main function to compute the interval:
+    Parameters:
+
+    x: number of observations/events
+
+    cl: confidence level to estimate, default value = 0.8413 (1-sigma)
+
+    step: size of steps (smaller more accurate), default value = 0.001
+    """
+    lower = x
+    while abs(sts.poisson.cdf(x-1,lower)-cl) > step:
+        lower -= step
+    
+    upper = x
+    while abs(1-sts.poisson.cdf(x,upper)-cl) > step:
+        upper += step
+    
+    return lower,upper
+
+CL = 0.8413
+Step = 0.001
+
+if len(sys.argv) == 2:
+    obs = eval(sys.argv[1])
+elif len(sys.argv) == 3:
+    obs = eval(sys.argv[1])
+    CL = eval(sys.argv[2])
+elif len(sys.argv) == 4:
+    obs = eval(sys.argv[1])
+    CL = eval(sys.argv[2])
+    Step = eval(sys.argv[3])
+elif len(sys.argv) > 4:
+    print 'Error: too many arguments.'
+    print 'To use: > gehrels <N_OBSERVATION> [CONFIDENCE INTERVAL] [STEP SIZE]'
+    quit()
+
+if len(sys.argv) < 2:
+    print 'To use: > gehrels <N_OBSERVATION> [CONFIDENCE INTERVAL] [STEP SIZE]'
+    print 'example: gehrels 34 0.84'
+    quit()
+
+res = gehrels(obs,CL,Step)
+print 'For',obs,'events,',CL,'confidence level limits are:'
+print res[0],res[1]
+
+
